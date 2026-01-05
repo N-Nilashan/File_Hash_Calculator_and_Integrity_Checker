fc26f110ccae823c51ff4bb473ae321cf608421c
G.S.Nimesh <informal.nimesh@gmail.com>
2026-01-05 22:53:28 +0530

    fixed bugs in saving process

--- hash_checker.py
+++ hash_checker.py
@@ -5,31 +5,24 @@ import json
 from colorama import init, Fore, Back, Style
 init(autoreset=True)
 
-# Create argument parser
+def get_file_hash(filename):
+    with open(filename,'rb') as f:
+        return hashlib.sha256(f.read()).hexdigest()
+
 parser = argparse.ArgumentParser(description="Hash Checker")
 
-# Argument for file input (required)
 parser.add_argument('-f', '--file', required=True, help="Enter the file name")
 
-# Flag to save hash to database
 parser.add_argument('-s', '--save', action='store_true', help="Save the hash code to Data Base")
 
-# Flag to verify file integrity
 parser.add_argument('-v', '--verify', action='store_true', help="Verify the integrity of the file")
 
-# Parse command-line arguments
 args = parser.parse_args()
 
 
-# If --save flag is used, attempt to save the file's hash
 if args.save:
-    with open(args.file, "rb") as file:
-        fileData = file.read()
-
-        # Generate SHA-256 hash of the file
-        hash_file = hashlib.sha256(fileData)
-        hash_code = hash_file.hexdigest()
-
+    hash_code = get_file_hash(args.file)
+    
 
 
     try:
@@ -42,7 +35,6 @@ if args.save:
     except FileNotFoundError:
         data = []  # empty database if file doesn't exist
 
-    # Boolean flag to track if the file is already in the database
     file_exists = any(item["filename"] == args.file for item in data)
 
     if not file_exists:
@@ -50,20 +42,13 @@ if args.save:
         with open("hash_database.json",'w') as db:
             json.dump(data,db,indent=4)
         print(Fore.GREEN + "Hash Code has been saved to the Data Base!")
-        # Print the generated hash
         print(hash_code)
     else:
         print(Fore.RED + f"{args.file} has already been saved!")
 
-# Verify file integrity if --verify flag is used
 if args.verify:
 
-    with open(args.file, "rb") as file:
-        fileData = file.read()
-
-        # Generate SHA-256 hash of the file
-        hash_file = hashlib.sha256(fileData)
-        hash_code = hash_file.hexdigest()
+    hash_code = get_file_hash(args.file)
 
     try:
         with open("hash_database.json", "r") as db:
